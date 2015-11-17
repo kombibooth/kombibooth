28d2a870f19ba3677f05af788b476ba90a24548a
Vinícius Krolow <krolow@gmail.com>
2015-11-17 20:28:26 -0200

    Add logo, rounded-box and button to home page

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 
 import Logo from '../components/layout/logo';
+import RoundedBox from '../components/layout/rounded-box';
+import Button from '../components/layout/button';
+
+import './home.scss';
 
 export default class Home extends Component {
   render () {
     return (
       <div>
-        <Logo type="horizontal" />
-        <Logo type="vertical" />
+        <RoundedBox>
+          <Logo type="vertical" />
+          <div className="button-tap-to-start">
+            <Button text="Tap to start" />
+          </div>
+        </RoundedBox>
       </div>
     );
   }
