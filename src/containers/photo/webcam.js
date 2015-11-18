de26666f4bf4784ff6cc787e0a415a2d0ca41851
Vinícius Krolow <krolow@gmail.com>
2015-11-18 02:05:52 -0200

    Add webcam and countdown to caputer photo
    - Add webcam component
    - Add countdown component
    
    Integrates handle between countdown trigger timer and redux action to persist captured photo

--- src/containers/photo/webcam.js
+++ src/containers/photo/webcam.js
@@ -1,15 +1,41 @@
-import React, { Component } from 'react';
-
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
 import Webcam from 'react-webcam';
+
+import CountDown from '../../components/photo/count-down';
+import { captured as photoCaptured } from '../../actions/photo';
 
 import './webcam.scss';
 
 export default class PhotoWebcam extends Component {
+
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+  };
+
+  handleOnCountDownFinish () {
+    this.props.dispatch(
+      photoCaptured(
+        this.refs.webcam.getScreenshot()
+      )
+    );
+
+    setTimeout(() => {
+      this.refs.countdown.reset();
+    }, 5000);
+  }
+
   render () {
     return (
       <div className="photo-webcam">
-        <Webcam width="1220" height="600" />
+        <CountDown
+          seconds={ 5 }
+          onCountDownFinish={ ::this.handleOnCountDownFinish }
+          ref="countdown" />
+        <Webcam width="1220" height="600" ref="webcam" />
       </div>
     );
   }
 }
+
+export default connect()(PhotoWebcam);
