c69a3e747c1bb6fb70b846c323aec172732b6052
Vinícius Krolow <krolow@gmail.com>
2015-12-07 17:55:46 -0200

    Add support to active and icon in button

--- src/components/layout/desktop/button.js
+++ src/components/layout/desktop/button.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import classNames from 'classnames';
 
-const Button = ({ children, className, size = 'large', type = 'default', onClick }) => {
+const Button = ({
+  children,
+  className,
+  size,
+  active,
+  icon,
+  type = 'default',
+  onClick,
+}) => {
   const btnClasses = classNames(
     'btn',
-    `btn-${size}`,
+    { size: `btn-${size}` },
+    { active },
     `btn-${type}`,
     className
   );
 
+  let iconTag;
+  if (icon) {
+    const iconClasses = classNames(
+      'icon',
+      `icon-${icon}`,
+    );
+
+    iconTag = (<span className={iconClasses}></span>);
+  }
+
   return (
     <button className={btnClasses} onClick={onClick}>
+      { iconTag }
       { children }
     </button>
   );
